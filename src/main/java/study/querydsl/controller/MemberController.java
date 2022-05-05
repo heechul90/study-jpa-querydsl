b987b91c4b292ba219a966e7357446e5b77a6989
hclee <heechul4296@gmail.com>
2022-05-06 00:23:14 +0900

    조회 API 컨트롤러 개발

---
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -0,0 +1,32 @@
+package study.querydsl.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping(value = "/v1/members")
+    public JsonResult searchMemberV1(MemberSearchCondition condition) {
+        List<MemberTeamDto> resultList = memberJpaRepository.searchByBuilder(condition);
+        return new JsonResult(resultList.size(), resultList);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class JsonResult<T> {
+        private int count;
+        private T data;
+    }
+}
