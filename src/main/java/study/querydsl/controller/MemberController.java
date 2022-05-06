83f7d0a08838e958c47e58ff8d0e11005e9d81c0
hclee <heechul4296@gmail.com>
2022-05-06 22:13:34 +0900

    스프링 데이터 페이징 활용3 - 컨트롤러 개발

--- src/main/java/study/querydsl/controller/MemberController.java
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -1,11 +1,14 @@
 package study.querydsl.controller;
 
 import lombok.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.repository.MemberJpaRepository;
+import study.querydsl.repository.MemberRepository;
 
 import java.util.List;
 
@@ -14,12 +17,26 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping(value = "/v1/members")
     public JsonResult searchMemberV1(MemberSearchCondition condition) {
         List<MemberTeamDto> resultList = memberJpaRepository.searchByBuilder(condition);
         return new JsonResult(resultList.size(), resultList);
     }
+
+    @GetMapping(value = "/v2/members")
+    public JsonResult searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        Page<MemberTeamDto> resultList = memberRepository.searchPageSimple(condition, pageable);
+        return new JsonResult(resultList.getSize(), resultList);
+    }
+
+    @GetMapping(value = "/v3/members")
+    public JsonResult searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        Page<MemberTeamDto> resultList = memberRepository.searchPageComplex(condition, pageable);
+        return new JsonResult(resultList.getSize(), resultList);
+    }
+
 
     @Data
     @AllArgsConstructor
